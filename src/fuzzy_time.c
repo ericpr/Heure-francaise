bdd5fb6b4bff211ad713e66e9ac7db4736ebdd41
Éric Proust <eric@progis.fr>
2016-07-14 09:03:45 +0200

    Contrôle de la taille des fenêtres

--- src/fuzzy_time.c
+++ src/fuzzy_time.c
@@ -13,10 +13,10 @@ static struct CommonWordsData {
 
 static void update_time(struct tm* t) {
   fuzzy_time_to_words(t->tm_hour, t->tm_min, s_data.buffer, BUFFER_SIZE);
-  //strcpy(s_data.label,"AAA");
-  //sprintf(s_data.buffer, "%d : %d",t->tm_hour, t->tm_min);
-  text_layer_set_text(s_data.label, s_data.buffer);
   fuzzy_date_to_words(t->tm_wday, t->tm_mday, t->tm_mon,t->tm_year, s_data.buffer2, BUFFER_SIZE);
+  //strcpy(s_data.buffer,"quatre heures moins vingt-cinq");
+  //strcpy(s_data.buffer2,"mercredi quatorze septembre");
+  text_layer_set_text(s_data.label, s_data.buffer);  
   text_layer_set_text(s_data.label2,s_data.buffer2);
 }
 
@@ -30,22 +30,22 @@ static void do_init(void) {
   window_stack_push(s_data.window, animated);
 
   window_set_background_color(s_data.window, GColorBlack);
-  //GFont font = fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD);
-  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_28);
+  GFont font = fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD);
+  //GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
   GFont font2 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
 
   Layer *root_layer = window_get_root_layer(s_data.window);
   GRect frame = layer_get_frame(root_layer);
 
-  s_data.label = text_layer_create(GRect(0, 5, frame.size.w, frame.size.h - 35));
-  text_layer_set_background_color(s_data.label, GColorBlack);
-  text_layer_set_text_color(s_data.label, GColorWhite);
+  s_data.label = text_layer_create(GRect(0, 5, frame.size.w, frame.size.h - 40));
+  text_layer_set_background_color(s_data.label, GColorWhite);
+  text_layer_set_text_color(s_data.label, GColorBlack);
   text_layer_set_font(s_data.label, font);
   layer_add_child(root_layer, text_layer_get_layer(s_data.label));
 
-  s_data.label2 = text_layer_create(GRect(0, frame.size.h -35, frame.size.w, frame.size.h - 5));
-  text_layer_set_background_color(s_data.label2, GColorBlack);
-  text_layer_set_text_color(s_data.label2, GColorWhite);
+  s_data.label2 = text_layer_create(GRect(0, frame.size.h -40, frame.size.w, frame.size.h - 5));
+  text_layer_set_background_color(s_data.label2, GColorWhite);
+  text_layer_set_text_color(s_data.label2, GColorBlack);
   text_layer_set_font(s_data.label2, font2);
   layer_add_child(root_layer, text_layer_get_layer(s_data.label2));
 
